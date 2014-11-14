8e7c1e5402476eb7fb1d8b644e16a6471ca4b2aa
Lucas Ruchel <lukasruchel@gmail.com>
2014-11-14 09:59:54 -0200

    startproject

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,1 @@
 
-function hideElements(){
-	elements = document.querySelectorAll("#container div");
-	nodes = elements.childNodes;
-
-	for (var i = 0; i < nodes.length; i++) {
-		Console.log(i);
-	};
-}
