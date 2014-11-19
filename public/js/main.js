a1c54c286ce7aed3e1610d433532762e8bcdda8c
Lucas Ruchel <lukasruchel@gmail.com>
2014-11-19 19:37:18 -0200

    Novas ideias, novos estilos

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,27 @@
-body = document.querySelector("#css-web-garden");
-popup = document.createElement("div");
-popup.classList.add("catch-fish");
+changeHeaderTitle();
 
-body.appendChild(popup);
+
+function changeHeaderTitle(){
+    element = document.querySelector(".header h1");
+    
+    container = document.createElement("div");
+    container.classList.add("header-title");
+    
+    element.innerHTML = "";
+    
+    word1 = document.createElement("span");
+    word1.appendChild(document.createTextNode("Jardim"));
+    word1.classList.add("top");
+    
+    word2 = document.createElement("span");
+    word2.appendChild(document.createTextNode("Web"));
+    word2.classList.add("bottom");
+    
+    element.appendChild(container);
+    
+    container.appendChild(word1);
+    container.appendChild(word2);
+}
+ function createNav(){
+     
+ }
