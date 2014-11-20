5151ef5ec4489f974ee0c792e8245fa3f2e180a9
Lucas Ruchel <lukasruchel@gmail.com>
2014-11-20 00:46:59 -0200

    menu quase pronto

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,20 @@
 changeHeaderTitle();
+createNav();
 
 
 function changeHeaderTitle(){
-    element = document.querySelector(".header h1");
+    var element = document.querySelector(".header h1");
     
-    container = document.createElement("div");
-    container.classList.add("header-title");
+    var container = document.createElement("div");
+        container.classList.add("header-title");
     
     element.innerHTML = "";
     
-    word1 = document.createElement("span");
+    var word1 = document.createElement("span");
     word1.appendChild(document.createTextNode("Jardim"));
     word1.classList.add("top");
     
-    word2 = document.createElement("span");
+    var word2 = document.createElement("span");
     word2.appendChild(document.createTextNode("Web"));
     word2.classList.add("bottom");
     
@@ -23,5 +24,38 @@ function changeHeaderTitle(){
     container.appendChild(word2);
 }
  function createNav(){
+    var element = document.querySelector(".header");
+    
+    var container = document.createElement("div");
+    container.classList.add("nav-bar");
+    
+    var path = "public/images/";
+    
+    var data =  '{"informations":[{"image" : "'+path+'img1.jpg","link"  : "#jar-preamble"}, {"image" : "img1.jpg","link"  : "jar-preamble"}]}';
+      
+            
+     objects = JSON.parse(data);
      
+    for (i=0;i<7;i++){
+        wrap = document.createElement("div");
+        wrap.classList.add("wrap");
+        
+        crop = document.createElement("div");
+        crop.classList.add("crop");
+        
+        link = document.createElement("a");
+        link.classList.add("nav-item");
+        link.href = objects.informations[0].link;
+        
+        img = document.createElement("img");
+        img.classList.add();
+        img.src = objects.informations[0].image;
+        
+        wrap.appendChild(crop);
+        crop.appendChild(link);
+        link.appendChild(img);
+        
+        container.appendChild(wrap);
+    }
+    element.appendChild(container);
  }
